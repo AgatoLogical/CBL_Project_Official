35ce24e518ab4d09cc63913146961918852eb9f2
Andreea V <andreeavasiliu460@gmail.com>
2023-10-12 20:36:26 +0200

    added component class

---
+++ Component.java
@@ -0,0 +1,14 @@
+//import 
+
+public class Component {
+    public String name;
+    public static final int value = 0;
+    // parents
+    // icon
+
+    // category
+
+    public String combine(String item1, String item2) {
+        return null;
+    }
+}
